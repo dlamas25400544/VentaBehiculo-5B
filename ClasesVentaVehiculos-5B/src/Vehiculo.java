c34f90bad8278447184cef46bfcad4a0e2fd62bc
Diego Alexander Lamas Villalobos <diallamasvi@ittepic.edu.mx>
2026-02-16 12:28:31 -0700

    feat: Clase Vehiculo de vehiculos

---
+++ ClasesVentaVehiculos-5B/src/Vehiculo.java
@@ -0,0 +1,21 @@
+
+public class Vehiculo {
+    private String marca;
+    private double precio;
+    private String motor;
+            
+    public Vehiculo(String marca, double precio, String motor){
+        this.marca=marca;
+        this.precio=precio;
+        this.motor=motor;
+    }
+    
+    @Override
+    public String toString(){
+        String cadena;
+        cadena="Marca: "+this.marca+"\n"
+                +"Precio: "+this.precio+"\n"
+                +"Motor: "+this.motor+"\n";
+        return motor;
+    }
+}
